a20d84ad4251feba561cf9243450a84925f6c042
G ROHITH LAKSHMAN <rohithlakshman999@gmail.com>
2026-09-19 00:52:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (72.04%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.c
@@ -0,0 +1,14 @@
+int findPeakElement(int* arr, int n) {
+    int left = 0;
+    int right = n - 1;
+    while(left < right) {
+        int mid = left + (right - left) / 2;
+        if(arr[mid] < arr[mid + 1]) {
+            left = mid + 1;
+        }
+        else {
+            right = mid;
+        }
+    }
+    return left;
+}
