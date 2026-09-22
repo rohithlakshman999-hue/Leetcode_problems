f84f436d998f9aae0c0d855eaa7f6f6f9ecc97a5
G ROHITH LAKSHMAN <rohithlakshman999@gmail.com>
2026-09-22 17:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (59.06%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def letterCombinations(self, dig):
+        list={
+            '2':"abc",
+            '3':"def",
+            '4':"ghi",
+            '5':"jkl",
+            '6':"mno",
+            '7':"pqrs",
+            '8':"tuv",
+            '9':"wxyz"
+        }
+        res = [""]
+
+        for digit in dig:
+            new = []
+
+            for old in res:
+                for ch in list[digit]:
+                    new.append(old + ch)
+
+            res = new
+
+        return res
