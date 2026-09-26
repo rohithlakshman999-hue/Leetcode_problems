2c1b0d1f8ece012db5baf184e8f6ac08a9a5435b
G ROHITH LAKSHMAN <rohithlakshman999@gmail.com>
2026-09-26 21:35:16 +0530

    Time: 7 ms (31.04%) | Memory: 12.4 MB (20.81%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x<2:
+            return x
+        l=1
+        r=x
+        while l<=r:
+            m=(l+r)//2
+            if m*m==x:
+                return m
+            elif m*m<x:
+                l=m+1
+            else:
+                r=m-1
+        return r
